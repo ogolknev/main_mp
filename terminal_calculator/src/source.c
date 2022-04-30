e012f3da2f569adbe7a7675cc6908a3cc42a2570
Ogolknev Nikita <ogolknev257@mail.ru>
2022-04-30 12:10:01 +0300

    change the algorithm of the program (add lists)
    now the data is processed using lists

--- terminal_calculator/src/source.c
+++ terminal_calculator/src/source.c
@@ -1,296 +1,385 @@
 /*
-	 __________________________________________________________________________________________________________________
-	/																												   \
-    |  Name        : Terminal Calculator																			   |
-	|  Author      : Ogolknev Nikita																				   |
-	|  Version     : 2.2																							   |
-	|  Description : this program is for calculating six arithmetic operations between two numbers					   |
-	|  Instruction : the program works with files, and for its work you need a file with ready-made input data.		   |
-	|	 		     in this file, in the line, the first character is an operation, then a space indicates			   |
-	|			     the use of individual numbers (s) or vectors (v) (for vectors, an indication of their dimensions) |
-	|			     and then the required amount of data, each of the numbers is also separated by a space.		   |
-	|			     there can be several lines in the file with the input data separated by a space or a newline.	   |
-	|			     the output file will contain the original expression, the equal sign and the result.			   |
-	|			     parentheses are used to designate vectors in the output file.                                     |
-	|          																									       |
-	|                more details in the README                                                                        |
-	\__________________________________________________________________________________________________________________/
+__________________________________________________________________________________________________________________
+
+ Name        : Terminal Calculator
+ Author      : Ogolknev Nikita
+ Version     : 2.2
+ Description : this program is for calculating six arithmetic operations between two numbers
+ Instruction : the program works with files, and for its work you need a file with ready-made input data.
+ 		       in this file, in the line, the first character is an operation, then a space indicates
+		       the use of individual numbers (s) or vectors (v) (for vectors, an indication of their dimensions)
+		       and then the required amount of data, each of the numbers is also separated by a space.
+		       there can be several lines in the file with the input data separated by a space or a newline.
+		       the output file will contain the original expression, the equal sign and the result.
+               parentheses are used to designate vectors in the output file.
+
+               more details in the README
+__________________________________________________________________________________________________________________
 
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-	setvbuf(stdout, NULL, _IONBF, 0);
-	setvbuf(stderr, NULL, _IONBF, 0);
-	//repeat - variable for repeating the program
-	char repeat = 'n';
-	//loop to repeat the program and select new input and output files
-	do
-	{
+//definition of functions for working with lists
+
+//structure for storing list item data
+struct input_data{
+    char operation;
+    char mode;
+    int size;
+    float *data;
+};
+
+//definition of new type - list
+typedef struct type_list{
+    struct input_data data;
+    struct type_list *next;
+}list;
+
+//creating new list
+list *newlist(struct input_data input_data){
+    list *newlist = malloc(sizeof(list));
+    newlist->data = input_data;
+    newlist->next = NULL;
+    return newlist;
+}
+
+//adding to beginning of list
+void addfirst(list **headlist, struct input_data input_data){
+    list *addfirst = newlist(input_data);
+    addfirst->next = *headlist;
+    *headlist = addfirst;
+}
+
+//adding to end of list
+void addlast(list **headlist, struct input_data input_data){
+    list *addlast = newlist(input_data);
+    list *currentlist = *headlist;
+    while(currentlist->next != NULL){
+        currentlist = currentlist->next;
+    }
+    currentlist->next = addlast;
+}
+
+//adding after a definite element
+void add(list **headlist, struct input_data input_data, int serialnum){
+    list *addlist = newlist(input_data);
+    list *currentlist = *headlist;
+    int current_serialnum = 0;
+    while(currentlist->next != NULL){
+        if(current_serialnum == serialnum){
+            addlist->next = currentlist->next;
+            currentlist->next = addlist;
+        }
+        current_serialnum++;
+        currentlist = currentlist->next;
+    }
+}
+
+
+//main function
+int main(int argc, char *argv[]){
+	//fix console output and input
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stderr, NULL, _IONBF, 0);
+    //repeat - variable for repeating the program
+    char repeat = 'n';
+    //loop to repeat the program and select new input and output files
+    do{
 		/*
 		variable description:
-		input - variable for working with file input.txt
-		output - variable for working with file output.txt
-		select - variable for selecting operating mode
-		operation - variable for selecting operation
+		input - variable for opening input file
+		output - variable for opening output file
 		path - variable to store path
 		*/
 		FILE *input, *output;
-		char *path, select, operation;
-		//allocation memory and opening input and output files
+		char *path;
+		//allocation memory
 		path = calloc(100, sizeof(char));
+		//entering path to input file
 		printf("enter the path to the file with input data:\n");
 		scanf(" %100s", path);
+		//opening input file
 		input = fopen(path, "r");
-		printf("enter the path to the file where the output will be placed:\n");
-		scanf(" %100s", path);
-		output = fopen(path, "w");
-		//loop processing input file to end
-		while(feof(input) == 0)
-		{
-			//selecting operation
-			fscanf(input, " %c", &operation);
-			//selecting operating mode
-			fscanf(input, " %c", &select);
+		//freeing memory
+		free(path);
+		//definition structure for storing list item data
+		struct input_data element;
+		//definition new list
+		list *input_data = newlist(element);
+		//reading an input file and saving the data to a list
+		while(feof(input) == 0){
+			fscanf(input, " %c", &element.operation);
+			fscanf(input, " %c", &element.mode);
+			if (element.mode == 'v'){
+				fscanf(input, "%i", &element.size);
+				element.data = malloc(element.size*sizeof(double));
+				for(int i=0; i < element.size*2; i++)
+				{
+					fscanf(input, "%f", &element.data[i]);
+				}
+				addlast(&input_data, element);
+			} else if(element.mode == 's') {
+				if(element.operation == '!'){
+					element.data = malloc(sizeof(double));
+					fscanf(input, "%f", &element.data[0]);
+				}
+				else {
+					element.data = malloc(2 * sizeof(double));
+					fscanf(input, "%f", &element.data[0]);
+					fscanf(input, "%f", &element.data[1]);
+				}
+				//adding new element to end of list
+				addlast(&input_data, element);
+			}
+		}
+		//closing file
+		fclose(input);
+		//definition new list
+		list *pointer = input_data;
+		//definition structure for storing list item data
+		struct input_data output_element;
+		//allocation memory
+		output_element.data = malloc(input_data->data.size*sizeof(double));
+		//definition new list
+		list *output_list = newlist(output_element);
+		//input
+		while(pointer->next != NULL) {
+			//move to next element of list
+			pointer = pointer->next;
+			//allocation memory
+			output_element.data = malloc(input_data->data.size*sizeof(double));
 			//vector mode
-			if(select == 'v')
+			if(pointer->data.mode == 'v')
 			{
-			/*
-			variable description:
-			vector_1 - variable to store the coordinates of the first vector
-			vector_2 - variable to store the coordinates of the second vector
-			result - variable to store resulting vector
-			res  - variable to store result of some operations
-			size - variable for selecting size of vectors used
-			*/
-			float *vector_1, *vector_2, *result, res = 0;
-			int size;
-			fscanf(input, "%i", &size);
-			//memory allocation
-			vector_1 = malloc(size*sizeof(float));
-			vector_2 = malloc(size*sizeof(float));
-			result = malloc(size*sizeof(float));
-			fprintf(output,"(");
-			//entering vectors and output of original expression
-			for(int i=0; i < size; i++)
-			{
-				fscanf(input, "%f", &vector_1[i]);
-				fprintf(output, " %f", vector_1[i]);
-			}
-			fprintf(output," ) ");
-			fprintf(output,"%c", operation);
-			fprintf(output," (");
-			for(int i=0; i < size; i++)
-			{
-				fscanf(input, "%f", &vector_2[i]);
-				fprintf(output, " %f", vector_2[i]);
-			}
-			fprintf(output," ) = ");
-			//calculation block
-			switch (operation)
-			{
-				//calculating and output of result one of operations
-				case '+':
-					fprintf(output, "( ");
-					for(int i=0; i < size; i++)
-					{
-						result[i] = vector_1[i] + vector_2[i];
-						fprintf(output, "%f ", result[i]);
-					}
-					fprintf(output, ")\n");
-					break;
-				//calculating and output of result one of operations
-				case '-':
-					fprintf(output, "( ");
-					for(int i=0; i < size; i++)
-					{
-						result[i] = vector_1[i] - vector_2[i];
-						fprintf(output, "%f ", result[i]);
-					}
-					fprintf(output, ")\n");
-					break;
-				//calculating and output of result one of operations
-				case '*':
-					fprintf(output, "( ");
-					for(int i=0; i < size; i++)
-					{
-						result[i] = vector_1[i] * vector_2[i];
-						fprintf(output, "%f ", result[i]);
-					}
-					fprintf(output, ")\n");
-					break;
-				//calculating and output of result one of operations
-				case '/':
-					fprintf(output, "( ");
-					for(int i=0; i < size; i++)
-					{
-						result[i] = vector_1[i] / vector_2[i];
-						fprintf(output, "%f ", result[i]);
-					}
-					fprintf(output, ")\n");
-					break;
-				//calculating and output of result one of operations
-				case '\'':
-					for(int i=0; i < size; i++)
-					{
-						res += vector_1[i] * vector_2[i];
-					}
-					fprintf(output, "%f\n", res);
-					break;
-				//calculating and output of result one of operations
-				case 'x':
-					if (size == 3)
-					{
-						fprintf(output, "( ");
-						fprintf(output, "%f ", (vector_1[1] * vector_2[2] - vector_1[2] * vector_2[1]));
-						fprintf(output, "%f ", -(vector_1[0] * vector_2[2] - vector_1[2] * vector_2[0]));
-						fprintf(output, "%f ", (vector_1[0] * vector_2[1] - vector_1[1] * vector_2[0]));
-						fprintf(output, ")\n");
-					}
-					//error warning
-					else
-					{
-						fprintf(output, "error\n");
-					}
-					break;
-				//error warning
-				default:
-					fprintf(output, "error\n");
-					break;
-			}
-			//freeing memory
-			free(vector_1);
-			free(vector_2);
-			free(result);
+				switch (pointer->data.operation)
+				{
+					//calculating and output of result one of operations
+					case '+':
+						for(int i=0; i < pointer->data.size; i++)
+						{
+							output_element.data[i] = pointer->data.data[i] + pointer->data.data[pointer->data.size+i];
+						}
+						break;
+					//calculating and output of result one of operations
+					case '-':
+						for(int i=0; i < pointer->data.size; i++)
+						{
+							output_element.data[i] = pointer->data.data[i] - pointer->data.data[pointer->data.size+i];
+						}
+						break;
+					//calculating and output of result one of operations
+					case '*':
+						for(int i=0; i < pointer->data.size; i++)
+						{
+							output_element.data[i] = pointer->data.data[i] * pointer->data.data[pointer->data.size+i];
+						}
+						break;
+					//calculating and output of result one of operations
+					case '/':
+						for(int i=0; i < pointer->data.size; i++)
+						{
+							output_element.data[i] = pointer->data.data[i] / pointer->data.data[pointer->data.size+i];
+						}
+						break;
+					//calculating and output of result one of operations
+					case '\'':
+						for(int i=0; i < pointer->data.size; i++)
+						{
+							output_element.data[0] += pointer->data.data[i] * pointer->data.data[pointer->data.size+i];
+						}
+						break;
+					//calculating and output of result one of operations
+					case 'x':
+						if (pointer->data.size == 3)
+						{
+							output_element.data[0] = pointer->data.data[1] * pointer->data.data[5] - pointer->data.data[4] * pointer->data.data[2];
+							output_element.data[1] = -(pointer->data.data[0] * pointer->data.data[5] - pointer->data.data[2] * pointer->data.data[3]);
+							output_element.data[2] = pointer->data.data[0] * pointer->data.data[4] - pointer->data.data[1] * pointer->data.data[3];
+						}
+						//error warning
+						else
+						{
+							output_element.operation = 'e';
+						}
+						break;
+						//error warning
+					default:
+						output_element.operation = 'e';
+						break;
+				}
 			}
 			//number mode
-			else if(select == 's')
+			else if(pointer->data.mode == 's')
 			{
 				/*
 				variables description:
-				degInd - degree indicator
-				num1 - first number
-				num2 - second number
+				deg - variable for calculation factorial degree
 				factorial - variable for calculation factorial
 				 */
-				int degInd;
-				float num1, num2, deg;
-				long long int factorial;
-				//reads a variable num1
-				fscanf(input, "%f", &num1);
+				int deg;
+				int factorial;
 				//checking for operations using 1 number
-				if (operation == '!' || operation == '^')
+				if (pointer->data.operation == '!' || pointer->data.operation == '^')
 				{
 					//operation selection
-					switch (operation)
+					switch (pointer->data.operation)
 					{
-					//factorial calculation block
+						//factorial calculation block
 						case '!':
 							factorial = 1;
 							//factorial negative number
-							if(num1 < 0)
+							if(pointer->data.data[0] < 0)
 							{
 								//error warning
-								fprintf(output, "error\n\"invalid value\"\n");
+								output_element.operation = 'e';
 							}
 							//factorial 0
-							else if(num1 == 0)
+							else if(pointer->data.data[0] == 0)
 							{
-								fprintf(output, "%.0f! = 1\n", num1);
+								output_element.data[0] = 1;
 							}
-							else
 							//main factorial
+							else
 							{
-								for(int i=1;i<=num1;i++)
+								for(int i=1; i <= pointer->data.data[0]; i++)
 								{
 									factorial=factorial*i;
 								}
-									//output
-								fprintf(output, "%.0f! = %I64i\n", num1, factorial);
+								//output
+								output_element.data[0] = factorial;
 							}
 							break;
 						//degree calculation and output
 						case '^':
-						deg = 1;
-						//reads a variable degInd
-						fscanf(input, "%i", &degInd);
-						//degree indicator 0
-						if(degInd == 0)
-						{
-							printf("%f^%i = 1\n",  num1, degInd);
-						}
-						//negative degree indicator
-						else if (degInd < 0)
-						{
-							for(int i=1;i<=-degInd;i++)
+							deg = 1;
+							//degree indicator 0
+							if(pointer->data.data[1] == 0)
 							{
-								deg = deg / num1;
+								output_element.data[0] = 1;
 							}
-							fprintf(output, "%f^%i = %f\n", num1, degInd, deg);
-						}
-						//main degree calculation
-						else
-						{
-							for(int i=1;i<=degInd;i++)
+							//negative degree indicator
+							else if (pointer->data.data[1] < 0)
 							{
-								deg = deg * num1;
+								for(int i=1;i <= pointer->data.data[1];i++)
+								{
+									deg = deg / pointer->data.data[0];
+								}
+								output_element.data[0] = deg;
 							}
-							fprintf(output, "%f^%i = %f\n", num1, degInd, deg);
-						}
-						break;
-					default:
-						//error warning
-						fprintf(output, "error\n");
-						break;
+							//main degree calculation
+							else
+							{
+								for(int i=1;i<=pointer->data.data[1];i++)
+								{
+									deg = deg * pointer->data.data[0];
+								}
+								output_element.data[0] = deg;
+							}
+							break;
+						default:
+							//error warning
+							output_element.operation = 'e';
+							break;
 					}
 				}
 				else
 				{
-					//reads a variable num2
-					fscanf(input, "%f", &num2);
 					//operation selection
-					switch (operation)
+					switch (pointer->data.operation)
 					{
-					//sum calculation
-					case '+':
-						fprintf(output, "%f + %f = %f\n", num1, num2, num1+num2);
-						break;
+						//sum calculation
+						case '+':
+							output_element.data[0] = pointer->data.data[0]+pointer->data.data[1];
+							break;
 						//subtraction calculation
 						case '-':
-						fprintf(output, "%f - %f = %f\n", num1, num2, num1-num2);
-						break;
-					//multiplication calculation
-					case '*':
-						fprintf(output, "%f * %f = %f\n", num1, num2, num1*num2);
-						break;
-					//division calculation
-					case '/':
-						fprintf(output, "%f / %f = %f\n", num1, num2, num1/num2);
-						break;
-					default:
-						//error warning
-						fprintf(output, "error\n\"unknown operation or incorrect input\"\n");
-						break;
+							output_element.data[0] = pointer->data.data[0]-pointer->data.data[1];
+							break;
+						//multiplication calculation
+						case '*':
+							output_element.data[0] = pointer->data.data[0]*pointer->data.data[1];
+							break;
+						//division calculation
+						case '/':
+							output_element.data[0] = pointer->data.data[0]/pointer->data.data[1];
+							break;
+						default:
+							//error warning
+							output_element.operation = 'e';
+							break;
 					}
 				}
 			}
 			//error warning
 			else
 			{
-				fprintf(output, "error\n\"incorrect input\"\n");
+				output_element.operation = 'e';
+			}
+			//adding new element to end of list
+			addlast(&output_list,output_element);
+		}
+		//entering path to output file
+		path = calloc(100, sizeof(char));
+		printf("enter the path to the file where the output will be placed:\n");
+		scanf(" %100s", path);
+		//opening output file
+		output = fopen(path, "w");
+		free(path);
+		//moving pointer to start
+		pointer = input_data;
+		//output
+		while(pointer->next != NULL){
+			//move to next element of list
+			pointer = pointer->next;
+			//move to next element of list
+			output_list = output_list->next;
+			//error warning
+			if(output_list->data.operation == 'e'){
+				fprintf(output, "error\n");
+			}
+			//output vector mode
+			else if(pointer->data.mode == 'v'){
+				fprintf(output, "( ");
+				for(int i = 0; i < pointer->data.size; i++){
+					fprintf(output, "%f ", pointer->data.data[i]);
+				}
+				fprintf(output, ")");
+				fprintf(output, " %c ", pointer->data.operation);
+				fprintf(output, "( ");
+				for(int i = 0; i < pointer->data.size; i++){
+					fprintf(output, "%f ", pointer->data.data[pointer->data.size + i]);
+				}
+				fprintf(output, ") = ");
+				if(pointer->data.operation == '\''){
+					fprintf(output,"%f\n", output_list->data.data[0]);
+				} else {
+					fprintf(output, "( ");
+					for(int i = 0; i < pointer->data.size; i++){
+						fprintf(output, "%f ", output_list->data.data[i]);
+					}
+					fprintf(output, ")\n");
+				}
+			//output number mode
+			} else if (pointer->data.mode == 's'){
+				if(pointer->data.operation == '!'){
+					fprintf(output, "%f! = %f\n", pointer->data.data[0], output_list->data.data[0]);
+				} else {
+					fprintf(output, "%f %c %f = %f\n", pointer->data.data[0],pointer->data.operation, pointer->data.data[1], output_list->data.data[0]);
+				}
 			}
 		}
-		//closing files
+		//closing output file
 		fclose(output);
-		fclose(input);
+		free(input_data);
+		free(pointer);
+		free(output_list);
 		//prompt to repeat the program and select new files
 		printf("select new files and start over?\n'y' - yes\nother - no\n");
 		scanf(" %c", &repeat);
-	}
-	while(repeat == 'y');
-	return 0;
+    }
+    while(repeat == 'y');
+    return 0;
 }
 //the end :)
